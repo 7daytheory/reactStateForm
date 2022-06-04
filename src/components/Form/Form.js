32e5859e45c375a4c62d7012fbf2dc7dcea2f830
Matthew Lowe <matthewjlowe11@gmail.com>
2022-06-04 12:16:32 -0400

    Removed unused React files, added form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import './Form.css';
+
+function Form() {
+    return (
+      <div className="Form">
+         <form>
+            Form here
+         </form>
+      </div>
+    );
+  }
+  
+  export default Form;
+  
