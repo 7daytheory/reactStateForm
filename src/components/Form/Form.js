35eb43b56fe7445cb82e892a5f3334b3326c6844
Matthew Lowe <matthewjlowe11@gmail.com>
2022-06-06 12:43:02 -0400

    Change input values using states when user types

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,68 @@
+import React, { Component } from 'react';
 import './Form.css';
 
-function Form() {
+class AddForm extends Component {
+   //set state for inputs
+    state = {
+      firstname: '',
+      email: '',
+      phone: '',
+   };
+
+   onInputChange = e => this.setState({ [e.target.name]:
+      e.target.value});
+   
+
+render() {
+   //Define states as variables
+   let { firstname, email, phone } = this.state;
     return (
-      <div className="Form">
-         <form>
-            Form here
-         </form>
+      <div className="card mb-3">
+         <div className="card-header">
+            Send Message
+         </div>
+         <div class="card-body">
+            <form>
+               <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                     type="text"
+                     className="form-control form-control-lg"
+                     placeholder="Enter Name..."
+                     required
+                     name="firstname"
+                     value={firstname}
+                     onChange={this.onInputChange}
+                  >
+                  </input>
+                  <label htmlFor="name">Email</label>
+                  <input
+                     type="text"
+                     className="form-control form-control-lg"
+                     placeholder="Enter email..."
+                     required
+                     name="email"
+                     value={email}
+                     onChange={this.onInputChange}
+                  >
+                  </input>
+                  <label htmlFor="name">Phone(optional)</label>
+                  <input
+                     type="text"
+                     className="form-control form-control-lg"
+                     placeholder="Enter phone number..."
+                     name="phone"
+                     value={phone}
+                     onChange={this.onInputChange}
+                  >
+                  </input>
+                  </div>
+            </form>
+         </div>
       </div>
     );
   }
+}
   
-  export default Form;
+  export default AddForm;
   
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Form from './components/Form/Form';
+import AddForm from './components/Form/Form';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <App />
-    <Form />
+    <AddForm />
   </div>
 );
